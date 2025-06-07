87347ebc61aa4845664f3c0edb65ce47a17d49e5
RaniaAfsharian <Rania.Afsharian@gmail.com>
2025-06-07 15:02:03 +0330

    Update PerkCard.cpp

--- PerkCard.cpp
+++ PerkCard.cpp
@@ -1,13 +1,30 @@
 #include "PerkCard.hpp"
+#include <sstream>
+#include <vector>
+
+using namespace std;
+
+vector<PerkCard> createPerkCards() {
+    vector<PerkCard> cards;
+    cards.emplace_back("Visit from the Detective", 3); 
+    cards.emplace_back("Break of Dawn", 3); 
+    cards.emplace_back("Overstock", 4); 
+    cards.emplace_back("Late into the Night", 4); 
+    cards.emplace_back("Repel", 3); 
+    cards.emplace_back("Hurry", 3); 
+    return cards;
+}
 
 PerkCard::PerkCard(string name, int quantity)
     : name(name), quantity(quantity) {}
 
-void PerkCard::activate() {
-    if (quantity > 0) {
-        --quantity;
-        // اثر کارت
+bool PerkCard::activate() {
+    if (quantity <= 0) {
+        return false; 
     }
+    --quantity; 
+    // اعمال کارت 
+    return true; 
 }
 
 string PerkCard::getDetails() const {
