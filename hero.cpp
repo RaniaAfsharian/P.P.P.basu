6d136c9ecda913947c2b5f5140a940c0dbd8247c
mediisa <mediisa598@gmail.com>
2025-06-08 21:05:43 +0330

    Update hero.cpp

--- hero.cpp
+++ hero.cpp
@@ -2,12 +2,12 @@
 
 #include "charactor.h"
 #include "hero.h"
+#include "Item.h"
+
 
 Hero::Hero(const std::string &name , const std::string &loc , HeroType htype):
-chara(name ,  loc , Typechara::HERO) , Act_max((htype==HeroType::Mayor) ? 5 : 4) , in_hospital(false)
-{
-HeroType(htype);
-}
+chara(name ,  loc , Typechara::HERO) , Act_max((htype==HeroType::Mayor) ? 5 : 4) , in_hospital(false) , htype(htype)
+{}
 
 
 int Hero::getAct_max()const{
@@ -41,7 +41,7 @@ void Hero::useAct(){
         Act_remaining=0;
     }
 }
-void Hero::send_hos(){
+void Hero::send_hos(){////////////تغییر دادن بیمارستاننننننن
     in_hospital=true;
     heal();
     Act_remaining=0;
@@ -58,13 +58,40 @@ void Hero::move_valliger(std::shared_ptr <chara> villager , const std::string& n
         useAct();
     }
 }
-
+void Hero::pickup_item( std::vector<std::shared_ptr<Item> > item){
+    if(Act_remaining>0){
+        items.push_back(item);
+        std::cout<<"Item picked:"<<items->getName()<<std::endl;
+        useAct();
+    }
+}
+void Hero::use_Item(const std::string& itemname){
+    auto it=std::find_if(item.begin() , item.end() , [&](auto &item)){
+        
+    }
+}
+/*
+void Hero::progress_inTask(std::shared_ptr <Monster> monster){
+    if(Act_remaining>0){
+        monstr.can_defeated();
+        useAct();
+    }
+}
+*/
+void Hero::defeatMonster(std::shared_ptr <Monster> monster){
+    if(Act_remaining>0){  
+        if(monster.can_defeated() && getLoc()==monster->getLoc()){
+            monster.can_defeat();
+            useAct();
+        }
+    }
+}
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Mayor::Mayor():Hero("mo" , "Theater" , HeroType::Mayor)
+Mayor::Mayor():Hero("MAYOR" , "Theater" , HeroType::Mayor)
 {}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Ancint::Ancint() : Hero("ASI" , "Docks" , HeroType::Ancint)
+Ancint::Ancint() : Hero("ANCInt" , "Docks" , HeroType::Ancint)
 {}
 
 void Ancint::takeSpcial_item(const std::string itemLoc , std::vector<std::string>& items){
