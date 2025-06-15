eabe0e6f43f0dd31d6d396508efd28514c7e2388
mediisa <mediisa598@gmail.com>
2025-06-15 12:17:59 +0330

    Update monsster.h

--- monsster.h
+++ monsster.h
@@ -7,77 +7,69 @@
 #include "Item.h"
 
 
-enum class monsterType{ INVISIBLE_MAN , DRACULA };
+#include <string>
+#include <vector>
+#include "character.h"
+#include "Item.h"
 
-class Monster : public chara{
-    private:
+enum class monsterType { INVISIBLE_MAN, DRACULA };
+
+class Monster : public chara {
+private:
     monsterType Mtype;
     std::string Npower;
     bool is_defeated;
     std::vector<std::string> targetLoc;
     std::vector<ItemType> itemType;
     int power;
-    //int requir_power;
-    //MonsterCard monstercard;
 
+public:
+    Monster(const std::string& name, const std::string& loc, monsterType Mtype,
+            const std::string& Npower, const std::vector<ItemType>& itemType, int power);
 
-    public:
-    Monster(const std::string& name , const std::string &loc , monsterType Mtype , const std::string& Npower , const std::vector<ItemType> itemtType , int power);
-
-    monsterType getMtype()const;
-    std::string getNpower()const;
-    bool getIs_defeated()const;
-    int getPower()const;
-
-    const std::vector<std::string>& getTargetLoc()const;
-    const std::vector<ItemType>& getItemType()const;
-   // MonsterCard getMonstercard()const;
+    monsterType getMtype() const;
+    std::string getNpower() const;
+    bool getIs_defeated() const;
+    int getPower() const;
+    const std::vector<std::string>& getTargetLoc() const;
+    const std::vector<ItemType>& getItemType() const;
 
     void setPower(int npower);
     void addTargetLoc(std::string LOCC);
     void defeat();
 
-    virtual void useSpecialP()=0;
-    virtual bool can_defeated()const=0;
-
-    bool checkItem_defeat(const std::vector<std::shared_ptr<ItemType>> &itemType) const;
-
-    bool can_defend()const override;
- 
+    virtual void useSpecialP() = 0;
+    virtual bool can_defeated() const = 0;
+    bool checkItem_defeat(const std::vector<Item>& items) const;
+    bool can_defend() const override;
 };
 
-
-class Dracula : public Monster{
-    private:
+class Dracula : public Monster {
+private:
     int coffins_marker;
 
-    public:
+public:
     Dracula(const std::string& loc);
-    
-    int getCoffins_marker()const;
 
-    void Coffin();//
-    bool allTakeCoffine()const; 
-
+    int getCoffins_marker() const;
+    void Coffin();
+    bool allTakeCoffin() const;
     void useSpecialP() override;
-    bool can_defeated()const override;
-
+    bool can_defeated() const override;
 };
 
-
-class Invisible_man : public Monster{
-    private:
+class Invisible_man : public Monster {
+private:
     int itemPlaced;
 
-    public:
+public:
     Invisible_man(const std::string& loc);
 
-    int getItemPlaced()const;
-    void ItemPlaced();///
-    bool allTakeItemPlaced()const;
-
+    int getItemPlaced() const;
+    void ItemPlaced();
+    bool allTakeItemPlaced() const;
     void useSpecialP() override;
-    bool can_defeated()const override;
+    bool can_defeated() const override;
 };
 
 #endif
