366076ac0609663dd1badf87d56a016097b2ab8e
mediisa <mediisa598@gmail.com>
2025-06-15 12:15:17 +0330

    Update monster.cpp

--- monster.cpp
+++ monster.cpp
@@ -1,129 +1,134 @@
-#include <bits/stdc++.h>
+#include <iostream>
 
-#include "charactor.h"
 #include "monster.h"
-#include "Item.h"
 
-Monster::Monster(const std::string& name , const std::string &loc , monsterType Mtype , const std::string& Npower , const std::vector<ItemType > itemType , int power) :
-chara(name , loc , Typechara::MONSTER) , is_defeated(false) ,Npower(Npower) , power(power) , Mtype(Mtype) , itemType(itemType)
-{}
 
-monsterType Monster::getMtype()const{
-    return Mtype;
-}
-std::string Monster::getNpower()const{
-    return Npower;
-}
-bool Monster::getIs_defeated()const{
-    return is_defeated;
-}
-int Monster::getPower()const{
-    return power;
-}
+Monster::Monster(const std::string& name, const std::string& loc, monsterType Mtype,
+                 const std::string& Npower, const std::vector<ItemType>& itemType, int power)
+    : chara(name, loc, Typechara::MONSTER), is_defeated(false), Npower(Npower),
+      power(power), Mtype(Mtype), itemType(itemType) {}
 
-const std::vector<std::string>& Monster::getTargetLoc()const{
-    return targetLoc;
+monsterType Monster::getMtype() const{
+    return Mtype; 
 }
-const std::vector<ItemType>& Monster::getItemType()const{
- return itemType;
+std::string Monster::getNpower() const{
+    return Npower; 
+}
+bool Monster::getIs_defeated() const{
+    return is_defeated; 
+}
+int Monster::getPower() const{
+    return power; 
+}
+const std::vector<std::string>& Monster::getTargetLoc() const{
+    return targetLoc; 
+}
+const std::vector<ItemType>& Monster::getItemType() const{
+    return itemType; 
 }
 
 void Monster::setPower(int npower){
-    power=npower;
+    power = npower; 
 }
-void Monster::addTargetLoc(std::string LOCC){
-    targetLoc.push_back(LOCC);
+void Monster::addTargetLoc(std::string LOCC) {
+    targetLoc.push_back(LOCC); 
 }
-void Monster::defeat(){
-    is_defeated=true;
+void Monster::defeat() {
+    is_defeated = true; 
 }
-/*
-bool Monster::checkItem_defeat(const std::vector<std::shared_ptr<Item>>& items) const {
+
+bool Monster::checkItem_defeat(const std::vector<Item>& items) const {
     int totalPower = 0;
-    std::vector<bool> typeFound(requiredItemTypes.size(), false);
+    bool allTypeFound=true;
+    std::vector<bool> typeFound(itemType.size(), false);
 
     for (const auto& item : items) {
-        for (size_t i = 0; i < requiredItemTypes.size(); ++i) {
-            if (item->getType() == requiredItemTypes[i]) {
+        for (size_t i = 0; i < itemType.size(); ++i) {
+            if (item.getType() == itemType[i] && !typeFound[i]) {
                 typeFound[i] = true;
-                totalPower += item->getPower();
+                totalPower += item.getEffect();
                 break;
             }
         }
     }
-    return std::all_of(typeFound.begin(), typeFound.end(), [](bool found) { return found; }) &&
-           totalPower >= requiredpower;
-}
-*/
-bool Monster::can_defend()const{
-    return false;
+
+    for(bool found : typeFound){
+        allTypeFound=(allTypeFound && found );
+    }
+
+    if(allTypeFound && totalPower >= power ){
+        return true;
+    }else{
+        return false;
+    }
+  
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Dracula::Dracula(const std::string& loc) : 
-Monster("Dracula " , loc , monsterType::DRACULA  , "count dracula" , {ItemType::Red , ItemType::Yellow } , 6 ) ,
-coffins_marker(0){
+bool Monster::can_defend() const{
+    return false; 
+}
+
+Dracula::Dracula(const std::string& loc)
+    : Monster("Dracula", loc, monsterType::DRACULA, "Count Dracula",
+            {ItemType::Red, ItemType::Yellow}, 6), coffins_marker(0) {
     addTargetLoc("Cave");
-    addTargetLoc("Dungeon"); 
+    addTargetLoc("Dungeon");
     addTargetLoc("Crypt");
     addTargetLoc("Graveyard");
 }
 
-
-int Dracula::getCoffins_marker()const{
-    return coffins_marker;
+int Dracula::getCoffins_marker() const{
+    return coffins_marker; 
 }
-void Dracula::Coffin(){
-    if(coffins_marker<4){
+void Dracula::Coffin() {
+    if (coffins_marker < 4){
         coffins_marker++;
     }
 }
-bool Dracula::allTakeCoffine()const{
-    if(coffins_marker<=4){
+bool Dracula::allTakeCoffin() const{
+    if(coffins_marker >= 4){
         return true;
     }else{
         return false;
     }
-} 
-void Dracula::useSpecialP(){
-    setPower(getPower()+2);
-    //افزایش قدرت دراکولا
 }
-bool Dracula::can_defeated()const {
-    return allTakeCoffine();
+void Dracula::useSpecialP() {
+    setPower(getPower() + 2);
+    std::cout << "Dracula's power increased!" <<std::endl;
+}
+bool Dracula::can_defeated() const{
+    return allTakeCoffin();
 }
 
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Invisible_man::Invisible_man(const std::string& loc):
-Monster("Invisidie_man" , loc , monsterType::INVISIBLE_MAN , "death anghel" , {ItemType::Red , ItemType::Yellow } , 9) ,itemPlaced(0)
-{
+Invisible_man::Invisible_man(const std::string& loc)
+    : Monster("Invisible Man", loc, monsterType::INVISIBLE_MAN, "Death Angel",
+              {ItemType::Red, ItemType::Yellow}, 9), itemPlaced(0) {
     addTargetLoc("Inn");
-    addTargetLoc("Barn"); 
+    addTargetLoc("Barn");
     addTargetLoc("Institute");
-    addTargetLoc("Laboratory");  
+    addTargetLoc("Laboratory");
     addTargetLoc("Mansion");
 }
 
-int Invisible_man::getItemPlaced()const{
-    return itemPlaced;
+int Invisible_man::getItemPlaced() const{
+    return itemPlaced; 
 }
-void Invisible_man::ItemPlaced(){/////////
-    if(itemPlaced<5){
+void Invisible_man::ItemPlaced() {
+    if (itemPlaced < 5) {
         itemPlaced++;
     }
 }
-bool Invisible_man::allTakeItemPlaced()const{
-    if(itemPlaced>=5){
+bool Invisible_man::allTakeItemPlaced() const {
+      if( itemPlaced >= 5){
         return true;
     }else{
         return false;
     }
 }
-
-void Invisible_man::useSpecialP(){
-    setPower(getPower()+5);
-        //افزایش قدرت مردنامرئی
+void Invisible_man::useSpecialP() {
+    setPower(getPower() + 5);
+    std::cout << "Invisible Man's power increased!" <<std::endl;
 }
-bool Invisible_man::can_defeated()const{
+bool Invisible_man::can_defeated() const {
     return allTakeItemPlaced();
 }
