59bcb8a3caafc1585e509de3ee581d01cef53dcb
mediisa <mediisa598@gmail.com>
2025-06-23 20:27:11 +0330

    Update ActionSystem.h

--- ActionSystem.h
+++ ActionSystem.h
@@ -1,64 +1,55 @@
-#ifndef ACTIONSYSTEM_HPP
-#define ACTIONSYSTEM_HPP
+#ifndef ACTIONSYSTEM_H
+#define ACTIONSYSTEM_H
 
 #include <string>
 #include <vector>
 #include <map>
 #include <set>
-#include <iostream>
+#include <memory>
 #include "hero.h"
+#include "character.h"
 #include "monster.h"
 #include "Item.h"
+#include "PerkCard.h"
+#include "Board.h"
 
-enum class ActionType {MOVE, GUIDE, PICK_UP, ADVANCE, DEFEAT, SPECIAL_ACTION };
+enum class ActionType {
+    MOVE,
+    GUIDE,
+    PICK_UP,
+    ADVANCE,
+    DEFEAT,
+    SPECIAL_ACTION
+};
 
 using Location = std::string;
 
-/*struct Item {
-    std::string name;
-    ItemType type;
-    Item(const std::string& name, ItemType type) : name(name), type(type) {}
-    std::string getName() const { return name; }
-};*/
-
-struct Person {
-    std::string name;
-    Location location;
-    bool isHero;
-    bool isInjured;
-    Typechara type; 
-    std::vector<Item> items;
-
-    Person(const std::string& name, const Location& loc, bool hero, Typechara type)
-        : name(name), location(loc), isHero(hero), isInjured(false), type(type) {}
-};
-
-/*struct PerkCard {
-    std::string description;
-    void applyEffect(Person& hero);
-};*/
-
 class ActionSystem {
-    std::map<std::string, Person> persons;
+private:
+    std::map<std::string, std::shared_ptr<chara>> persons;
     std::map<Location, std::set<std::string>> locations;
     std::vector<PerkCard> activePerks;
     int heroActionsLeft;
-    Person* currentHero;
+    std::shared_ptr<Hero> currentHero;
+    Board& board;
+  
 
-    void movePerson(Person& p, const Location& newLoc);
+    void movePerson(std::shared_ptr<chara> p, const Location& newLoc);
     bool isNeighbor(const Location& from, const Location& to);
 
 public:
-    ActionSystem(const std::vector<Person>& heroes);
-
+    ActionSystem(const std::vector<std::shared_ptr<chara>>& persons,Board& board);
+    ActionSystem(Board& board);
+    ActionSystem();
+    
     void startHeroTurn(const std::string& heroName, int actionsCount);
     void addPerkCard(const PerkCard& card);
     void playPerkCard(int index);
 
-    bool performAction(ActionType action);
+    bool performAction(ActionType action , const std::vector<std::shared_ptr<Monster>>& monsters);
 
     int getActionsLeft() const;
-    void showStatus();
+    void showStatus() const;
 };
 
-#endif 
+#endif
