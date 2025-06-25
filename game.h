be9514d3f2292ac304e5b59c54fac7addeb7afd1
mediisa <mediisa598@gmail.com>
2025-06-25 15:38:46 +0330

    Update game.h

--- game.h
+++ game.h
@@ -1,46 +1,44 @@
 #ifndef GAME_H
 #define GAME_H
 
-
 #include <bits/stdc++.h>
 
+
 #include "ActionSystem.h"
-#include "board.h"
-#include "charactor.h"
+#include "Board.h"
+#include "character.h"
 #include "Item.h"
 #include "hero.h"
 #include "monster.h"
-#include "MonsterCard.h"
 #include "PerkCard.h"
 #include "TerrorLevelTracker.h"
 
-
 class Game {
-    private:
+private:
     Board gameBoard;
     TerrorLevelTracker gameTerrorLevelTracker;
     ActionSystem gameActionSystem;
+    std::vector<PerkCard> gamePerkCard;
     std::vector<std::shared_ptr<Hero>> gameHero;
     std::vector<std::shared_ptr<Monster>> gameMonster;
     std::vector<std::shared_ptr<villager>> gamevillager;
-    std::vector<std::shared_ptr<Item>> gameitem;
+    std::vector<Item> gameitem;
 
     int playerindex;
     bool gameOver;
 
-    public:
+public:
     Game();
-    
+
     void setGame();
     void loop_Game();
     void heroGame(Hero& hero);
     void monsterGame();
-    void chech_win();
+    void check_win();
     void show_Gamestate();
-    
-
-
-
+    void show_menu();
+    void savegame_state();
+    void loadgame_state();
 };
 
 #endif
