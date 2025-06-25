950ee39d9f8d1e9a0d60306c53043bf0555381a2
mediisa <mediisa598@gmail.com>
2025-06-25 15:41:56 +0330

    Update PerkCard.cpp

--- PerkCard.cpp
+++ PerkCard.cpp
@@ -1,17 +1,18 @@
-#include "PerkCard.hpp"
+#include <iostream>
 #include <sstream>
 #include <vector>
+#include "PerkCard.h"
 
 using namespace std;
 
 vector<PerkCard> createPerkCards() {
     vector<PerkCard> cards;
-    cards.emplace_back("Visit from the Detective", 3); 
-    cards.emplace_back("Break of Dawn", 3); 
-    cards.emplace_back("Overstock", 4); 
-    cards.emplace_back("Late into the Night", 4); 
-    cards.emplace_back("Repel", 3); 
-    cards.emplace_back("Hurry", 3); 
+    cards.emplace_back("Visit from the Detective", 3);
+    cards.emplace_back("Break of Dawn", 3);
+    cards.emplace_back("Overstock", 4);
+    cards.emplace_back("Late into the Night", 4);
+    cards.emplace_back("Repel", 3);
+    cards.emplace_back("Hurry", 3);
     return cards;
 }
 
@@ -19,17 +20,17 @@ PerkCard::PerkCard(const std::string& name, int quantity)
     : name(name), quantity(quantity) {}
 
 bool PerkCard::activate() {
-    if (quantity <= 0) {
-        return false; 
-    }
-    --quantity; 
-    return true; 
+    if (quantity <= 0) return false;
+    quantity--;
+    return true;
 }
 
-void PerkCard::applyEffect(Person& hero) {
-    std::cout << "Special card application: " << name << " for " << hero.name << "\n";
+void PerkCard::applyEffect(Hero& hero) {
+    cout << "Applying perk card: " << name << " for " << hero.getName() << endl;
 }
 
 string PerkCard::getDetails() const {
-    return "PerkCard Name: " + name + ", Quantity: " + to_string(quantity);
+    stringstream ss;
+    ss << "Perk card: " << name << ", quantity: " << quantity;
+    return ss.str();
 }
