0a570d30deca510b90b43139e84e7fb14e5db0a1
RaniaAfsharian <Rania.Afsharian@gmail.com>
2025-07-03 21:28:01 +0330

    Add files via upload

--- Item.cpp
+++ Item.cpp
@@ -1,62 +1,20 @@
+// File: Item.cpp
 #include <bits/stdc++.h>
-
 #include "Item.h"
 
-using namespace std;
+Item::Item(const std::string& name, ItemType type) : name(name), type(type) {}
 
-vector<Item> createItems() {
-    vector<Item> items;
-    items.emplace_back("Flower_Docks", ItemType::Yellow, 2, 2);
-    items.emplace_back("TarotDeck_Camp", ItemType::Yellow, 3, 2);
-    items.emplace_back("Garlic_Inn", ItemType::Yellow, 2, 2);
-    items.emplace_back("MirroredBox_Mansion", ItemType::Yellow, 3, 2);
-    items.emplace_back("Stake_Abbey", ItemType::Yellow, 3, 2);
-    items.emplace_back("ScrollOfThoth_Museum", ItemType::Yellow, 4, 2);
-    items.emplace_back("Violin_Camp", ItemType::Yellow, 3, 2);
-    items.emplace_back("Tablet_Museum", ItemType::Yellow, 3, 2);
-    items.emplace_back("Wolfsbane_Camp", ItemType::Yellow, 4, 2);
-    items.emplace_back("Charm_Camp", ItemType::Yellow, 4, 2);
-    items.emplace_back("Dart_Inn", ItemType::Red, 2, 2);
-    items.emplace_back("FirePoker_Mansion", ItemType::Red, 3, 2);
-    items.emplace_back("Rapier_Theatre", ItemType::Red, 5, 2);
-    items.emplace_back("Shovel_Graveyard", ItemType::Red, 2, 2);
-    items.emplace_back("Torch_Barn", ItemType::Red, 5, 2);
-    items.emplace_back("Pitchfork_Barn", ItemType::Red, 4, 2);
-    items.emplace_back("Rifle_Barn", ItemType::Red, 6, 2);
-    items.emplace_back("SilverCane_Shop", ItemType::Red, 6, 2);
-    items.emplace_back("Knife_Docks", ItemType::Red, 3, 2);
-    items.emplace_back("Pistol_Precinct", ItemType::Red, 6, 2);
-    items.emplace_back("BearTrap_Shop", ItemType::Red, 4, 2);
-    items.emplace_back("Speargun_Institute", ItemType::Red, 4, 2);
-    items.emplace_back("AnatomyText_Institute", ItemType::Blue, 1, 2);
-    items.emplace_back("Centrifuge_Laboratory", ItemType::Blue, 1, 2);
-    items.emplace_back("Kite_Tower", ItemType::Blue, 1, 2);
-    items.emplace_back("Research_Tower", ItemType::Blue, 2, 2);
-    items.emplace_back("Telescope_Mansion", ItemType::Blue, 2, 2);
+std::string Item::getName() const { return name; }
+
+ItemType Item::getType() const { return type; }
+
+std::vector<Item> createItems() {
+    std::vector<Item> items;
+    items.emplace_back("Red1", ItemType::Red);
+    items.emplace_back("Red2", ItemType::Red);
+    items.emplace_back("Red3", ItemType::Red);
+    items.emplace_back("Yellow1", ItemType::Yellow);
+    items.emplace_back("Yellow2", ItemType::Yellow);
+    items.emplace_back("Yellow3", ItemType::Yellow);
     return items;
-}
-
-Item::Item(const std::string& name, ItemType itemType, int effect, int quantity)
-    : name(name), type(itemType), effect(effect), quantity(quantity) {}
-
-bool Item::useItem() {
-    if (quantity <= 0) {
-        cout << "No quantity left for item: " << name << endl;
-        return false;
-    }
-    quantity--;
-    cout << "Item used: " << name << endl;
-    return true;
-}
-
-std::string Item::getDetails() const {
-    stringstream ss;
-    ss << "Item: " << name << ", Type: ";
-    switch (type) {
-        case ItemType::Red: ss << "Red"; break;
-        case ItemType::Blue: ss << "Blue"; break;
-        case ItemType::Yellow: ss << "Yellow"; break;
-    }
-    ss << ", Effect: " << effect << ", Quantity: " << quantity;
-    return ss.str();
 }
--- Item.h
+++ Item.h
@@ -1,40 +1,20 @@
+// File: Item.h
 #ifndef ITEM_H
 #define ITEM_H
 
-
 #include <bits/stdc++.h>
 
-enum class ItemType { Red, Blue, Yellow };
+enum class ItemType { Red, Yellow };
 
 class Item {
 private:
     std::string name;
     ItemType type;
-    int effect;
-    int quantity;
 
 public:
-    Item(const std::string& name, ItemType itemType, int effect, int quantity);
-
-    Item(const Item&) = default;
-    Item(Item&&) = default;
-    Item& operator=(const Item&) = default;
-    Item& operator=(Item&&) = default;
-
-    bool useItem();
-    std::string getDetails() const;
-    std::string getName() const {
-        return name;
-    }
-    ItemType getType() const {
-        return type;
-    }
-    int getEffect() const {
-        return effect;
-    }
-    int getQuantity() const {
-        return quantity;
-    }
+    Item(const std::string& name, ItemType type);
+    std::string getName() const;
+    ItemType getType() const;
 };
 
 std::vector<Item> createItems();
