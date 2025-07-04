bbbe494591522b802a58ddb3e40d1c2f73b0dab9
RaniaAfsharian <Rania.Afsharian@gmail.com>
2025-07-04 10:19:32 +0330

    Add files via upload

--- hero.cpp
+++ hero.cpp
@@ -1,142 +1,64 @@
+// File: hero.cpp
 #include <bits/stdc++.h>
-
 #include "hero.h"
+#include "Board.h"
 
+Hero::Hero(const std::string& name, const std::string& loc, HeroType hType)
+    : chara(name, loc, Typechara::HERO), hType(hType), actions(3), Act_max(3), Act_remaining(3), in_hospital(false) {}
 
-Hero::Hero(const std::string& name, const std::string& loc, HeroType htype)
-    : chara(name, loc, Typechara::HERO), Act_max((htype == HeroType::Mayor) ? 5 : 4),
-      Act_remaining(0), in_hospital(false), htype(htype) {}
+int Hero::getAct_max() const { return Act_max; }
+int Hero::getAct_rem() const { return Act_remaining; }
+bool Hero::getin_hos() const { return in_hospital; }
 
-int Hero::getAct_max() const{
-    return Act_max; 
-}
-
-int Hero::getAct_rem() const{
-    return Act_remaining; 
-}
-
-bool Hero::getin_hos() const{ 
-    return in_hospital; 
-}
-
-HeroType Hero::getHType() const{
-    return htype; 
-}
-
-const std::vector<Item>& Hero::getItems() const{
-    return items; 
-}
-
-void Hero::setAct_max(int a){
-    Act_max=a;
-}
-
-void Hero::setAct_rem(int a){
-    Act_remaining=a;
-}
-
-bool Hero::setin_hos(bool a){
-    in_hospital=a;
-}
+void Hero::setAct_max(int a) { Act_max = a; }
+void Hero::setAct_rem(int a) { Act_remaining = a; }
+void Hero::setin_hos(bool a) { in_hospital = a; }
 
 void Hero::startTurn() {
-    if (in_hospital) {
-        heal();
-        in_hospital = false;
-    }
     Act_remaining = Act_max;
+    in_hospital = false;
 }
 
 void Hero::heal() {
-    setHealth(100); 
-}
-
-void Hero::useAct() {
-    if (Act_remaining > 0) {
-        Act_remaining--;
-    }
+    std::cout << name << " healed!" << std::endl;
 }
 
 void Hero::send_hos() {
     in_hospital = true;
-    heal();
     Act_remaining = 0;
+    std::cout << name << " sent to hospital!" << std::endl;
 }
 
 void Hero::move(const std::string& newLoc) {
-    if (Act_remaining > 0) {
-        useAct();
-        setLoc(newLoc);
-    }
+    setLoc(newLoc);
+    std::cout << name << " moved to " << newLoc << std::endl;
 }
 
 void Hero::move_valliger(std::shared_ptr<chara> villager, const std::string& newLoc) {
-    if (Act_remaining > 0 && villager->getType() == Typechara::VILLAGER) {
-        villager->setLoc(newLoc);
-        useAct();
-    }
-}
-
-void Hero::pickup_item(const Item& item) {
-    if (Act_remaining > 0) {
-        items.push_back(item);
-        std::cout << "Item picked up: " << item.getDetails() <<std::endl;
-        useAct();
-    }
+    villager->setLoc(newLoc);
+    std::cout << villager->getName() << " moved to " << newLoc << std::endl;
 }
 
 void Hero::use_Item(const std::string& itemname) {
-    // اگر عملیتی باقی نمانده یا آیتم خالی است، برگرد
-    if (Act_remaining <= 0 || itemname.empty()) {
-        return;
-    }
-
-    // جستجوی آیتم در لیست
-    for (size_t i = 0; i < items.size(); ++i) {
-        if (items[i].getName() == itemname) {
-            // استفاده از آیتم
-            if (items[i].useItem()) {
-                std::cout << "Used item: " << itemname << std::endl;
-                
-                // اگر تعداد آیتم به صفر رسید، از لیست حذف شود
-                if (items[i].getQuantity() <= 0) {
-                    items.erase(items.begin() + i);
-                }
-                
-                useAct();
-            } else {
-                std::cout << "Item cannot be used: " << itemname << std::endl;
-            }
-            return; // پس از پیدا کردن آیتم، حلقه را پایان بده
-        }
-    }
-
-    std::cout << "Item not found: " << itemname << std::endl;
-}
-
-void Hero::defeatMonster(std::shared_ptr<Monster> monster) {
-    if (Act_remaining > 0) {
-        if (monster->can_defeated() && getLoc() == monster->getLoc()) {
-            monster->defeat();
-            useAct();
-            std::cout << "Monster defeated!" <<std::endl;
-        } else {
-            std::cout << "Cannot defeat the monster!" <<std::endl;
+    for (auto it = items.begin(); it != items.end(); ++it) {
+        if (it->getName() == itemname) {
+            items.erase(it);
+            std::cout << name << " used item " << itemname << std::endl;
+            break;
         }
     }
 }
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-Mayor::Mayor() : Hero("Mayor", "Theatre", HeroType::Mayor)
-{}
+Mayor::Mayor() : Hero("Mayor", "Precinct", HeroType::Mayor) {}
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-Ancient::Ancient() : Hero("Ancient", "Docks", HeroType::Ancient)
-{}
+Ancient::Ancient() : Hero("Ancient", "Museum", HeroType::Ancient) {}
 
-void Ancient::takeSpcial_item(const std::string& itemLoc, std::vector<std::string>& items) {
-    if (getAct_rem() > 0) {
-        std::cout << "Ancient used special ability to pick up item!" <<std::endl;
-        useAct();
+void Ancient::takeSpcial_item(const std::string& itemLoc, Board& board) {
+    auto items = board.getItems();
+    auto it = items.find(itemLoc);
+    if (it != items.end() && !it->second.empty()) {
+        pickup_item(it->second[0]);
+        board.removeItems(itemLoc, it->second[0]);
+        std::cout << "Ancient picked up special item at " << itemLoc << std::endl;
     }
 }
--- hero.h
+++ hero.h
@@ -1,45 +1,41 @@
-
+// File: hero.h
 #ifndef HERO_H
 #define HERO_H
 
-#include <bits/stdc++.h>
-
 #include "character.h"
 #include "monster.h"
-#include "Item.h"
 
-enum class HeroType { Mayor, Ancient };
+enum class HeroType { Ancient, Mayor };
 
 class Hero : public chara {
-private:
-    HeroType htype;
+protected:
+    HeroType hType;
+    std::vector<Item> items;
+    int actions;
     int Act_max;
     int Act_remaining;
     bool in_hospital;
 
 public:
-    Hero(const std::string& name, const std::string& loc, HeroType htype);
-
+    Hero(const std::string& name, const std::string& loc, HeroType hType);
+    HeroType getHType() const { return hType; }
+    std::vector<Item> getItems() const { return items; }
+    void pickup_item(const Item& item) { items.push_back(item); }
+    void useAct() { if (actions > 0) actions--; }
+    void takeHarm(int damage) { /* فرضی: کاهش سلامت */ }
+    void defeatMonster(std::shared_ptr<Monster> monster) { /* فرضی: منطق شکست هیولا */ }
     int getAct_max() const;
     int getAct_rem() const;
     bool getin_hos() const;
-    HeroType getHType() const;
-    const std::vector<Item>& getItems() const;
-
     void setAct_max(int a);
     void setAct_rem(int a);
-    bool setin_hos(bool a);
-
-
+    void setin_hos(bool a);
     void startTurn();
     void heal();
-    void useAct();
     void send_hos();
     void move(const std::string& newLoc);
     void move_valliger(std::shared_ptr<chara> villager, const std::string& newLoc);
-    void pickup_item(const Item& item);
     void use_Item(const std::string& itemname);
-    void defeatMonster(std::shared_ptr<Monster> monster);
 };
 
 class Mayor : public Hero {
@@ -50,7 +46,7 @@ public:
 class Ancient : public Hero {
 public:
     Ancient();
-    void takeSpcial_item(const std::string& itemLoc, std::vector<std::string>& items);
+    void takeSpcial_item(const std::string& itemLoc, Board& board);
 };
 
 #endif
